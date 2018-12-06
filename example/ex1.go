fc90b4c68e2e62fccb5d069c806635324d9080db
tfbrother <tfbrother@qq.com>
2018-12-06 10:24:06 +0800

    修改测试文件中的bug     原测试代码中存在参数数量错误，类型错误，方法不存在

--- example/ex1.go
+++ example/ex1.go
@@ -2,25 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/petar/GoLLRB/llrb"
+	"github.com/tfbrother/GoLLRB/llrb"
 )
 
-func lessInt(a, b interface{}) bool { return a.(int) < b.(int) }
-
 func main() {
-	tree := llrb.New(lessInt)
-	tree.ReplaceOrInsert(1)
-	tree.ReplaceOrInsert(2)
-	tree.ReplaceOrInsert(3)
-	tree.ReplaceOrInsert(4)
+	tree := llrb.New()
+	tree.ReplaceOrInsert(llrb.Int(1))
+	tree.ReplaceOrInsert(llrb.Int(2))
+	tree.ReplaceOrInsert(llrb.Int(3))
+	tree.ReplaceOrInsert(llrb.Int(4))
 	tree.DeleteMin()
-	tree.Delete(4)
-	c := tree.IterAscend()
-	for {
-		u := <-c
-		if u == nil {
-			break
-		}
-		fmt.Printf("%d\n", int(u.(int)))
-	}
+	//tree.Delete(llrb.Int(4))
+	fmt.Println(tree.Max())
+	fmt.Println(tree.Min())
 }
